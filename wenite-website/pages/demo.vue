b4a6445a7d73827afd5f705685a15c83310b2a06
soneoso <eli.dekeersgieter@gmail.com>
2022-12-06 10:00:26 +0100

    Adds demo - changes to index

---
+++ wenite-website/pages/demo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="flex items-center justify-center h-screen flex-col ">
+            <h1 class="text-blue-600 text-9xl">Book A Demo.</h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
