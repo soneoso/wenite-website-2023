853e1286ecfad1daa919293122b3915da2c837eb
soneoso <eli.dekeersgieter@gmail.com>
2022-12-06 09:58:20 +0100

    Adds Header component

---
+++ wenite-website/components/home/Header.vue
@@ -0,0 +1,33 @@
+<template>
+<header class="">
+    <div class="container mx-auto px-6 py-24 text-center flex bg-white">
+        <div class="mx-auto max-w-xl">
+            <h1 class="opacity-100 drop-shadow-sm shadow-black  text-3xl font-bold text-gray-600 md:text-8xl">
+                Increase <span class="text-blue-500">Wellbeing</span>, Backed by <span class="text-gray-600 ">Data</span>
+            </h1>
+                <p class="mt-6 text-gray-500 dark:text-gray-600 md:text-2xl">
+                    We enable organizations to understand how their employees are feeling
+                    and how they are experiencing their employment.
+                </p>
+            <nuxt-link to="https://app.wenite.io/login">
+                <button type="button" class="header-btn-book"
+                    >Get started free
+                </button>
+            </nuxt-link>
+            
+        </div>
+    </div>
+</header>
+</template>
+
+<script>
+
+    export default {
+        
+
+    }
+</script>
+
+<style  scoped>
+
+</style>
