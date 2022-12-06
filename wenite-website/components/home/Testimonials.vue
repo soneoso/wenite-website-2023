2a0abbbd5fc7a160aebed21e79d11ce6d1520f47
soneoso <eli.dekeersgieter@gmail.com>
2022-12-06 10:00:09 +0100

    Adds Testimonials - Footer - About

---
+++ wenite-website/components/home/Testimonials.vue
@@ -0,0 +1,127 @@
+<template>
+<div class="min-w-screen min-h-screen flex items-center justify-center py-5">
+    <div class="w-full bg-white border-t border-b border-gray-200 px-5 py-16 md:py-24 text-gray-800">
+        <div class="w-full max-w-6xl mx-auto">
+            <div class="text-center max-w-xl mx-auto">
+                <h1 class="text-6xl md:text-7xl font-bold mb-5 text-gray-600">What people <br>are saying.</h1>
+                <h3 class="text-xl mb-5 font-light"></h3>
+                <div class="text-center mb-10">
+                    <span class="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                    <span class="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                    <span class="inline-block w-40 h-1 rounded-full bg-indigo-500"></span>
+                    <span class="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                    <span class="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                </div>
+            </div>
+            <div class="-mx-3 md:flex items-start">
+                <div class="px-3 md:w-1/3">
+                    <div class="w-full mx-auto rounded-full bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                    <div class="p-8">
+                        <div class="w-full flex mb-4 items-center">
+                            <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                <img src="https://i.pravatar.cc/100?img=1" alt="">
+                            </div>
+                            <div class="flex-grow pl-3">
+                                <h6 class="font-bold text-sm uppercase text-gray-600">Elke Verstraeten</h6>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>We work with Wenite because it is the only solultion that offered clear benefits ofr the organizations, the HR team and the employees.<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                        </div>
+                    </div>
+                </div>
+                    <div class="w-full mx-auto rounded-full bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                    <div class="p-8">
+                        <div class="w-full flex mb-4 items-center">
+                            <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                <img src="https://i.pravatar.cc/100?img=1" alt="">
+                            </div>
+                            <div class="flex-grow pl-3">
+                                <h6 class="font-bold text-sm uppercase text-gray-600">Elke Verstraeten</h6>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>We work with Wenite because it is the only solultion that offered clear benefits ofr the organizations, the HR team and the employees<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="px-3 md:w-1/3">
+                    <div class="w-full mx-auto rounded-full bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                    <div class="p-8">
+                        <div class="w-full flex mb-4 items-center">
+                            <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                <img src="https://i.pravatar.cc/100?img=1" alt="">
+                            </div>
+                            <div class="flex-grow pl-3">
+                                <h6 class="font-bold text-sm uppercase text-gray-600">Elke Verstraeten</h6>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>We work with Wenite because it is the only solultion that offered clear benefits ofr the organizations, the HR team and the employees.<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                        </div>
+                    </div>
+                </div>
+                    <div class="w-full mx-auto rounded-full bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                    <div class="p-8">
+                        <div class="w-full flex mb-4 items-center">
+                            <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                <img src="https://i.pravatar.cc/100?img=1" alt="">
+                            </div>
+                            <div class="flex-grow pl-3">
+                                <h6 class="font-bold text-sm uppercase text-gray-600">Elke Verstraeten</h6>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>We work with Wenite because it is the only solultion that offered clear benefits ofr the organizations, the HR team and the employees<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                 <div class="px-3 md:w-1/3">
+                    <div class="w-full mx-auto rounded-full bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                    <div class="p-8">
+                        <div class="w-full flex mb-4 items-center">
+                            <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                <img src="https://i.pravatar.cc/100?img=1" alt="">
+                            </div>
+                            <div class="flex-grow pl-3">
+                                <h6 class="font-bold text-sm uppercase text-gray-600">Elke Verstraeten</h6>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>We work with Wenite because it is the only solultion that offered clear benefits ofr the organizations, the HR team and the employees.<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                        </div>
+                    </div>
+                </div>
+                    <div class="w-full mx-auto rounded-full bg-white border border-gray-200 p-5 text-gray-800 font-light mb-6">
+                    <div class="p-8">
+                        <div class="w-full flex mb-4 items-center">
+                            <div class="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                <img src="https://i.pravatar.cc/100?img=1" alt="">
+                            </div>
+                            <div class="flex-grow pl-3">
+                                <h6 class="font-bold text-sm uppercase text-gray-600">Elke Verstraeten</h6>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <p class="text-sm leading-tight"><span class="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>We work with Wenite because it is the only solultion that offered clear benefits ofr the organizations, the HR team and the employees<span class="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span></p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
--- wenite-website/pages/about.vue
+++ wenite-website/pages/about.vue
@@ -4,6 +4,11 @@
             <h1 class="text-blue-600 text-9xl">About.</h1>
         </div>
     </div>
+        <div>
+        <div class="flex items-center justify-center h-screen flex-col ">
+            <h1 class="text-blue-600 text-9xl">About.</h1>
+        </div>
+    </div>
 </template>
 
 <script setup>
