db2671d77d5287951a81b569bfe101f9b1e4ff32
soneoso <eli.dekeersgieter@gmail.com>
2022-12-06 09:59:49 +0100

    adds Features - Partners - ProductPlacement

---
+++ wenite-website/components/home/Features.vue
@@ -0,0 +1,131 @@
+<template>
+            <div class="flex justify-center items-center">
+            <img src="~/assets/images/header-rightside.png" alt="">
+            </div>
+    <div class="max-w-screen-2xl rounded-3xl mx-auto bg-slate-50 shadow-2xl p-32 text-gray-600">
+            <div class="text-cente text-center max-w-xl mx-auto py-16">
+                <h1 class="text-6xl font-bold py-8 max-w-xl">Powered by Human Rescourses, People, Data</h1>
+                <p class="text-gray-500 dark:text-gray-600 md:text-xl">A data-first platform that centralizes all wellbeing content, services and insights</p>
+            </div>
+                            <div class="text-center mb-10">
+                    <span class="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                    <span class="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                    <span class="inline-block w-40 h-1 rounded-full bg-indigo-500"></span>
+                    <span class="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                    <span class="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1"></span>
+                </div>
+            <div class="lg:pl-16">
+                <div class="flex flex-col lg:flex-row space-y-16">
+                    <section class="flex flex-col justify-center pb-8">
+                        <!-- <p class="text-blue-400">HR</p> -->
+                        <h2 class="text-4xl font-bold">
+                            Realtime Wellbeing Metrics
+                        </h2>
+                            <p class="pt-6 md:text-2xl font-light">
+                                We provide you with up-to-date information.
+                            </p>
+                                <ul class="flex flex-col py-8 gap-3">
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li class="">Need help with privacy laws around the world?</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>Regulation monitoring</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>It’s battle tested. Our clients have been audited.</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>Guided configuration</li>
+                                    </div>
+                                </ul>
+                            <div class="">
+                                <button class="nav-btn-start-free">Wellbeing Metrics</button>
+                            </div>
+                        </section>
+                    <img src="~/assets/images/organization-metrics-rightside.png" alt="" class="lg:block hidden">
+                </div>
+
+                <div class="flex flex-col lg:flex-row space-y-16">
+                    <section class="flex flex-col justify-center order-2 pb-8">
+                    
+                        <h2 class="text-4xl font-bold">
+                            Get Something in Return
+                        </h2>
+                            <p class="pt-6 md:text-2xl font-light">
+                                Share insights and boost your wellbeing.
+                            </p>
+                                <ul class="flex flex-col py-8 gap-3">
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li class="">Need help with privacy laws around the world?</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>Regulation monitoring</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>It’s battle tested. Our clients have been audited.</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>Guided configuration</li>
+                                    </div>
+                                </ul>
+                            <div class="">
+                                <button class="nav-btn-start-free">boost your wellbeing</button>
+                            </div>
+                        </section>
+                        <img class="order-1 pr-32 lg:block hidden" src="~/assets/images/organization-interpretation-rightside.png" alt="">
+                    
+                </div>
+                        <div class="flex flex-col lg:flex-row space-y-16">
+                    <section class="flex flex-col justify-center pb-8 ">
+                        <h2 class="text-4xl font-bold">
+                            Personal Guidance
+                        </h2>
+                            <p class="pt-6 md:text-2xl font-light max-w-lg">
+                                Based on your personal interests and struggles we offer personalized guidance and solutions
+                            </p>
+                                <ul class="flex flex-col py-8 gap-3">
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li class="">Need help with privacy laws around the world?</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>Regulation monitoring</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>It’s battle tested. Our clients have been audited.</li>
+                                    </div>
+                                    <div class="flex">
+                                        <p class="material-icons">check</p>
+                                        <li>Guided configuration</li>
+                                    </div>
+                                </ul>
+                            <div class="">
+                                <button class="nav-btn-start-free">guidance and solutions</button>
+                            </div>
+                        </section>
+                    <img src="~/assets/images/organization-personal-guidance-rightside.png" alt="" class="lg:block hidden pl-32">
+                    </div>
+                    </div>
+            </div>
+            
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ wenite-website/components/home/Partners.vue
@@ -0,0 +1,86 @@
+<template>
+<section class="flex flex-col justify-center h-screen">
+        <div class="max-w-9xl py-16 text-center">
+            <span class="text-5xl md:text-6xl font-bold text-gray-600 ">Our Partnerships</span>
+            <p class="mt-6 text-gray-500 dark:text-gray-600 md:text-2xl">Have confidence in the wellbeing strategy that you are running. Wenite gives HR teams data and tools to work with employees.</p>
+        </div>
+        <div class="max-w-screen-xl px-4 py-8 mx-auto lg:pb-16">
+            <div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 sm:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                    <a href="#" class="flex items-center lg:justify-center">
+                        <img src="~/assets/images/BDO_logo.svg.png" class="h-10 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35" fill="currentColor">
+                    </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/Dpg-media-Logo.svg.png" class="h-12 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 86 29" fill="currentColor">                                              
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/logo-attentia.png" class="h-12 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 151 34" fill="currentColor">                                                              
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/vandemoortele.svg.png" class="h-9 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 124 38" fill="currentColor">                                                                                 
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/sioen_coating_quadri-224x120.png" class="h-12  hover:text-gray-900 dark:hover:text-white" viewBox="0 0 137 37" fill="currentColor">                                                                                
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/logo_text.png" class="h-12 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 124 21" fill="currentColor">
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/vdab.png" class="h-10 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35" fill="currentColor">
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/immobel.png" class="h-12 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 86 29" fill="currentColor">                                              
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/hogeschoolantwerpen.png" class="h-12 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 151 34" fill="currentColor">                                                              
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/bp-logo.png" class="h-12 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 124 38" fill="currentColor">                                                                                 
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/wdp-logo.png" class="h-12  hover:text-gray-900 dark:hover:text-white" viewBox="0 0 137 37" fill="currentColor">                                                                                
+                </a>
+                </div>
+                <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-3xl shadow-md hover:bg-blue-100 dark:hover:bg-blue-100">
+                <a href="#" class="flex items-center lg:justify-center">
+                    <img src="~/assets/images/xerius-logo.png" class="h-12 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 124 21" fill="currentColor">
+                </a>
+                </div>
+            </div>
+        </div>
+            <div class="drop-shadow-sm py-16 text-5xl md:text-5xl font-bold flex flex-row gap-8 text-center justify-center text-gray-600">
+                <span>30+ Partners</span>
+                <span>1.2k+ Users</span>
+            </div>
+</section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
