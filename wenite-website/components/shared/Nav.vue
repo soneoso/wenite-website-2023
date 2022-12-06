ff40c5c81b5e08e9c7131ee4d53893c66eafba7b
soneoso <eli.dekeersgieter@gmail.com>
2022-12-06 09:58:44 +0100

    Adds Nav component

--- wenite-website/components/shared/Nav.vue
+++ wenite-website/components/shared/Nav.vue
@@ -1,44 +1,71 @@
 <template>
-    <header class="sticky top-8 z-40">
-                <nav x-data="{ isOpen: false }" class="container border-[1px] rounded-full bg-white drop-shadow-2xl mt-8 mx-auto  p-4 lg:flex lg:items-center lg:justify-between max-w-7xl">
+    <header class="sticky top-8 z-40 px-8">
+        <nav class="container m-8 mx-auto border-[1px] rounded-full bg-white drop-shadow p-4 lg:flex lg:items-center lg:justify-between max-w-7xl flex-wrap">
             <div class="flex items-center justify-between">
             <div>
-                <nuxt-link class="text-2xl pl-3 font-bold text-gray-800 hover:text-gray-700 dark:text-blue-600 dark:hover:text-gray-300 lg:text-3xl" to="/">Wenite</nuxt-link>
+                <nuxt-link class="text-2xl pl-3 font-bold  text-gray-600 dark:hover:text-gray-300 lg:text-3xl" to="/">wenite</nuxt-link>
             </div>
             <!-- Mobile menu button -->
             <div class="flex lg:hidden">
                 <button x-cloak @click="isOpen = !isOpen" type="button" class="text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none dark:text-gray-200 dark:hover:text-gray-400 dark:focus:text-gray-400" aria-label="toggle menu">
-                <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
-                </svg>
-
-                <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
                 </button>
             </div>
             </div>
             <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-            <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out dark:bg-gray-900 lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none lg:dark:bg-transparent">
-            
-            <div class="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
+            <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out dark:bg-white lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center rounded-b-3xl lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none lg:dark:bg-transparent ">
 
-                <nuxt-link class="navbtn" to="/about">About</nuxt-link>
-                <nuxt-link class="navbtn" to="/features">Features</nuxt-link>
-                <nuxt-link class="navbtn" to="/cases">Use Cases</nuxt-link>
-                <nuxt-link class="navbtn" to="/pricing">Prices</nuxt-link>
-            </div>
-
-            </div>
-                <div class="flex flex-col space-y-4 lg:space-x-3 lg:flex-row lg:space-y-0">
-                    <nuxt-link class="nav-btn-login" to="/login">Login</nuxt-link>
-                    <nuxt-link class="nav-btn-contact" to="/contact">Contact Us</nuxt-link>
+            <ul class="flex flex-col lg:space-x-28 lg:flex-row">
+                <div class="flex items-center flex-col justify-center space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
+                    <li><nuxt-link class="navbtn" to="/about">About</nuxt-link></li>
+                    <li><nuxt-link class="navbtn" to="/features">Features</nuxt-link></li>
+                    <li><nuxt-link class="navbtn" to="/cases">Use Cases</nuxt-link></li>
+                    <li><nuxt-link class="navbtn" to="/pricing">Prices</nuxt-link></li>
                 </div>
+                <div class="flex flex-col justify-center lg:space-x-3 lg:flex-row">
+                        <li><nuxt-link class="nav-btn-login" to="https://app.wenite.io/login">Login</nuxt-link></li>
+                        <li><nuxt-link class="nav-btn-start-free hidden xl:block" to="https://app.wenite.io/login">Get started free</nuxt-link></li>
+                </div>
+            </ul>
+
+            </div>
         </nav>
+
     </header>
 </template>
 
-<script setup>
+<script>
+
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    drawer() {
+      this.isOpen = !this.isOpen;
+    }
+  },
+  watch: {
+    isOpen: {
+      immediate: true,
+      handler(isOpen) {
+        if (process.client) {
+          if (isOpen) document.body.style.setProperty("overflow", "hidden");
+          else document.body.style.removeProperty("overflow");
+        }
+      }
+    }
+  },
+  mounted() {
+    document.addEventListener("keydown", e => {
+      if (e.keyCode == 27 && this.isOpen) this.isOpen = false;
+    });
+  }
+};
 
 </script>
 
