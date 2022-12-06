ab4296d00fb537ac21b3a381515040a4ffaf611a
soneoso <eli.dekeersgieter@gmail.com>
2022-12-06 10:00:43 +0100

    Adds login temp - signup temp

--- wenite-website/pages/login.vue
+++ wenite-website/pages/login.vue
@@ -1,9 +1,69 @@
 <template>
-    <div>
-        <div class="flex items-center justify-center h-screen flex-col ">
-            <h1 class="text-blue-600 text-9xl">Login.</h1>
+<!DOCTYPE html>
+<html>
+    <body class>
+        <div class="lg:flex">
+            <div class="lg:w-1/2 xl:max-w-screen-sm">
+                <div class="py-12 bg-indigo-100 lg:bg-white flex justify-center lg:justify-start lg:px-12">
+                    <div class="cursor-pointer flex items-center">
+                        <div>
+                            <svg class="w-10 text-indigo-500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 225 225" style="enable-background:new 0 0 225 225;" xml:space="preserve">
+                                <g transform="matrix( 1, 0, 0, 1, 0,0) ">
+                                <g>
+                                <path id="Layer0_0_1_STROKES" class="st0" d="M173.8,151.5l13.6-13.6 M35.4,89.9l29.1-29 M89.4,34.9v1 M137.4,187.9l-0.6-0.4     M36.6,138.7l0.2-0.2 M56.1,169.1l27.7-27.6 M63.8,111.5l74.3-74.4 M87.1,188.1L187.6,87.6 M110.8,114.5l57.8-57.8"/>
+                                </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="text-2xl text-indigo-600 tracking-wide ml-2 font-semibold">Wenite.</div>
+                    </div>
+                </div>
+                <div class="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+                    <h2 class="text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
+                    xl:text-bold">Log in</h2>
+                    <div class="mt-12">
+                        <form>
+                            <div>
+                                <div class="text-sm font-bold text-gray-700 tracking-wide">Email Address</div>
+                                <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="mike@gmail.com">
+                            </div>
+                            <div class="mt-8">
+                                <div class="flex justify-between items-center">
+                                    <div class="text-sm font-bold text-gray-700 tracking-wide">
+                                        Password
+                                    </div>
+                                    <div>
+                                        <a class="text-xs font-display font-semibold text-indigo-600 hover:text-indigo-800
+                                        cursor-pointer">
+                                            Forgot Password?
+                                        </a>
+                                    </div>
+                                </div>
+                                <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="Enter your password">
+                            </div>
+                            <div class="mt-10">
+                                <button class="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                shadow-lg">
+                                    Log In
+                                </button>
+                            </div>
+                        </form>
+                        <div class="mt-12 text-sm font-display font-semibold text-gray-700 text-center">
+                            Don't have an account ? <a class="cursor-pointer text-indigo-600 hover:text-indigo-800">Sign up</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="hidden lg:flex items-center justify-center bg-indigo-100 flex-1 h-screen">
+                <div class="max-w-xs transform duration-200 hover:scale-110 cursor-pointer">
+
+                </div>
+            </div>
         </div>
-    </div>
+    </body>
+</html>
+
 </template>
 
 <script setup>
