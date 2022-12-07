6b6e8f94b08ce542e6d4840fbce19fdb90ad4b1d
soneoso <eli.dekeersgieter@gmail.com>
2022-12-07 20:44:46 +0100

    adds new comps

---
+++ wenite-website/pages/solutions.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="flex items-center justify-center h-screen flex-col ">
+            <h1 class="text-black text-9xl">Solutions</h1>
+        </div>
+    </div>
+    <HomeJoin />
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
