33249e69848d7df3b366001a251da7c146f63d14
soneoso <eli.dekeersgieter@gmail.com>
2022-12-12 09:42:42 +0100

    Create - Collection1&2- pricing & contact header.

---
+++ wenite-website/components/pricing/PricingHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ArrowNarrowRightIcon } from "@vue-hero-icons/outline"
+</script>
+
+
+<template>
+    <div>
+        <div class="container flex flex-col mx-auto px-6 py-32 items-center">
+
+            <h1 class="text-black font-inter shrink font-semibold text-9xl text-center">Simple and transparent pricing</h1>
+            <div>
+                <p class="mt-6 text-gray-500 dark:text-gray-600 md:text-2xl">
+                    Choose what you like! (We just love letting people make their own choices)
+                </p>
+                <!-- <p class="text-center mx-auto items-center pt-8 dark:text-gray-600">Are you allready with us and need specific information about the Wenite dashboard?</p>
+                <div class="flex mx-auto justify-center">
+                    <nuxt-link to="https://app.wenite.io/login">
+                        <button type="button" class="header-btn-book">Go to our Help Center 
+                            <Icon name="system-uicons:arrow-right" class="-mt-1" size="28" />
+                        </button>
+                    </nuxt-link>
+                </div> -->
+            </div>
+        </div>
+    </div>
+</template>
