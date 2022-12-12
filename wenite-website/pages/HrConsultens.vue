abc21d34cb66ed7b9f60721effe5a134037f8800
soneoso <eli.dekeersgieter@gmail.com>
2022-12-12 15:36:56 +0100

    Add new pages to flow navbar - Code clean up

---
+++ wenite-website/pages/HrConsultens.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>
+            <div class="flex items-center justify-center h-screen flex-col">
+                <h1 class="text-black text-9xl">Hr Consultens</h1>
+                <p class="mt-6 text-gray-500 dark:text-gray-600 md:text-2xl">
+                    Learn more about our story and the hard-working people behind the powerfull
+                </p>
+                <span class="font-bold md:text-2xl">Wellbeing Management Platform.</span>
+            </div>
+        </div>
+        <div>
+            <div class="flex items-center justify-center h-screen flex-col ">
+                <h1 class="text-black text-9xl">Learn more about our story and the hard-working people behind the
+                    beautiful Consent Management Platform.</h1>
+            </div>
+        </div>
+        <HomeJoin />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
