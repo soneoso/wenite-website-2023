2bb80d6cce18b831f92945892a641dc42ff9abf0
soneoso <eli.dekeersgieter@gmail.com>
2022-12-13 09:03:05 +0100

    New styling for Header - Home

--- wenite-website/components/home/Header.vue
+++ wenite-website/components/home/Header.vue
@@ -1,31 +1,38 @@
 <template>
-<header class="">
-    <div class="container mx-auto px-6 py-24 text-center flex bg-white">
-        <div class="mx-auto max-w-2xl ">
-            <h1 class="opacity-100 drop-shadow-sm shadow-black text-3xl font-semibold  md:text-9xl">
-                Increase <span class="text-">wellbeing</span>, backed by <span class=" ">data</span>
-            </h1>
-                <p class="mt-6 text-gray-500 dark:text-gray-600 md:text-2xl">
-                    We enable organizations to understand how their employees are feeling
-                    and how they are experiencing their employment.
+    <header class="">
+        <div class="container mx-auto px-6 py-24 flex bg-white">
+            <div class=" max-w-4xl">
+                <h1 class="opacity-100 drop-shadow-sm shadow-black text-3xl font-semibold  md:text-9xl">
+                    A personalized approach to employee experience
+                </h1>
+                <p class="mt-6 max-w-3xl text-gray-500 dark:text-gray-600 md:text-2xl">
+                    Wenite delivers an incredibly personalized employee experience using AI technologies — your
+                    data-driven hub activates employee sentiments and helps you build tailored experiences.
                 </p>
-            <nuxt-link to="https://app.wenite.io/login">
-                <button type="button" class="header-btn-book"
-                    >Get started free
-                    <Icon name="system-uicons:arrow-right" class="-mt-1" size="28" />
-                </button>
-            </nuxt-link>
+                <nuxt-link to="https://app.wenite.io/login">
+                    <button type="button" class="header-book-demo">Book a demo
+                        <Icon name="system-uicons:arrow-right" class="-mt-1" size="28" />
+                    </button>
+                    <button type="button" class="header-btn-book">Get started free
+                        <Icon name="system-uicons:arrow-right" class="-mt-1" size="28" />
+                    </button>
+                </nuxt-link>
+            </div>
+            <div class="mx-auto justify-center pt-32 items-center rounded-full">
+                <img src="~/assets/images/tryout2.gif" class="" alt="">
+            </div>
+            
         </div>
-    </div>
-</header>
+        
+    </header>
 </template>
 
 <script>
 
-    export default {
-        
+export default {
 
-    }
+
+}
 </script>
 
 <style  scoped>
