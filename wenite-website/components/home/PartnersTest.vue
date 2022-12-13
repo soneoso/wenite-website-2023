b3d4729a4469d5a0aac859ddaa735d0ba713fa29
soneoso <eli.dekeersgieter@gmail.com>
2022-12-13 09:01:59 +0100

    Adds & deletes - assets - tailwind changes

---
+++ wenite-website/components/home/PartnersTest.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bg-gradient-to-r from-zinc-800 to-zinc-900">
+        <div class="container mx-auto px-6 py-24 flex text-center justify-center items-center">
+            <div class="max-w-4xl">
+                <h2 class="opacity-100 drop-shadow-sm shadow-black text-3xl text-white font-semibold  md:text-7xl">
+                +100 clients personalize their employee experience using Wenite
+                </h2>
+
+
+            </div>
+            <div class="mx-auto justify-center pt-32 items-center rounded-full">
+
+            </div>
+            
+        </div>
+        <div class="grid lg:grid-cols-5 md:grid-cols-3 gap-16 px-36 py-16">
+        <img src="~/assets/images/immobel-white-logo.png" class="">
+        <img src="~/assets/images/bdo-white-logo.png" class="pt-24 pl-8">
+        <img src="~/assets/images/sportoase-white-logo.png" class="pt-16 pr-16">
+        <img src="~/assets/images/bp-white-logo.png" class="pt-14">
+        <img src="~/assets/images/fiberklaar-white-logo.png" class="pt-16">
+        </div>
+</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
