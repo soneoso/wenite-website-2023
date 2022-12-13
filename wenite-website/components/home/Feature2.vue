773a241e7ed74ab3cea0670e9c8773c58ecf8640
soneoso <eli.dekeersgieter@gmail.com>
2022-12-13 12:13:41 +0100

    Add new pages - Features

---
+++ wenite-website/components/home/Feature2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container flex h-screen mx-auto px-6 py-36 items-center  bg-white">
+        <div class=" max-w-1xl">
+            <h1 class="opacity-100 drop-shadow-sm shadow-black text-8xl font-semibold  md:text-7xl">
+                Centralization results in personalization.
+            </h1>
+            <p class="mt-6 max-w-3xl text-gray-500 dark:text-gray-600 md:text-3xl ">
+                Wenite acts as a single-access point for your employees to consume all content and offerings related to
+                their personal development and wellbeing. Employees share on average 10 data points per week with our
+                recommender systems. Why? We offer value to employees.
+            </p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ wenite-website/components/home/Feature3.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container flex h-screen mx-auto px-6 py-36 items-center  bg-white">
+        <div class=" max-w-1xl">
+            <h1 class="opacity-100 drop-shadow-sm shadow-black text-8xl font-semibold  md:text-7xl">
+                Tailored to the needs of your organization.
+            </h1>
+            <p class="mt-6 max-w-3xl text-gray-500 dark:text-gray-600 md:text-3xl ">
+                Integrations within your company culture and tooling ecosystem are facilitated by our vast network of
+                partners. Wenite's platform is designed to maximize the impact of your current best practices, our
+                customer success team is here to guide to toward success.
+            </p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
