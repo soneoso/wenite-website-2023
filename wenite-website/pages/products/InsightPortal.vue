e91edaee666edb7531772a862b00d328e0f9541b
soneoso <eli.dekeersgieter@gmail.com>
2022-12-14 13:46:58 +0100

    Adds pages - Emplo - Insight - Consu - Team

---
+++ wenite-website/pages/products/InsightPortal.vue
@@ -0,0 +1,52 @@
+<template>
+    <header class="">
+        <div class="container h-screen mx-auto px-6 py-48 flex bg-white">
+            <div class=" max-w-4xl">
+                <h1 class="opacity-100 drop-shadow-sm shadow-black text-3xl font-semibold  md:text-9xl">
+                    Insight Portal
+                </h1>
+                <p class="mt-6 max-w-3xl text-gray-500 dark:text-gray-600 md:text-3xl">
+                    Wenite provides the data and tools to put employee wellbeing at the heart of your organization.
+                    Our smart technology delivers insights that you can instantly put into action.
+                </p>
+                <nuxt-link to="https://app.wenite.io/login">
+                    <button type="button" class="header-book-demo">Book a demo
+                        <Icon name="system-uicons:arrow-right" class="-mt-1" size="28" />
+                    </button>
+                    <button type="button" class="header-btn-book">Get started free
+                        <Icon name="system-uicons:arrow-right" class="-mt-1" size="28" />
+                    </button>
+                </nuxt-link>
+            </div>
+<div class="flex mx-auto ">
+
+                <div class="border grid place-items-center rounded-full shadow-2xl bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 " style="height: 500px; width: 500px; font-size: 170px" >
+                    <h1 class="text-3xl font-semibold"></h1>
+                    <!-- <div class="mt-4 mb-10 "> -->
+                    <!-- <p class="text-gray-600">Visit our Employee support page if your company is using our product or
+                        service </p> -->
+                    <!-- </div> -->
+                    <!-- <div class="flex pt-8 justify-end">
+                    <Icon name="mdi:arrow-right-thin" size="50" />
+                </div> -->
+                </div>
+
+
+        </div>
+
+        </div>
+
+    </header>
+</template>
+
+<script>
+
+export default {
+
+
+}
+</script>
+
+<style  scoped>
+
+</style>
